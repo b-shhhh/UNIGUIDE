a5a113a2667c854734cf07eef717b88908d915b3
b-shhhh <159143153+b-shhhh@users.noreply.github.com>
2026-02-11 10:34:04 +0545

    database error

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -2,5 +2,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5050;
-export const MONGODB_URI: string = process.env.MONGODB_URI || 'mongodb://localhost:27017/university_guide';
+export const MONGODB_URI: string = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/university_guide';
 export const JWT_SECRET: string = process.env.JWT_SECRET || 'default_secret';
