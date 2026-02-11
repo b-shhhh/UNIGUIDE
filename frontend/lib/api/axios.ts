87cfef2bc22a2336249b81b25ed8a3afd6923ccd
b-shhhh <159143153+b-shhhh@users.noreply.github.com>
2026-02-11 14:19:30 +0545

    ok

--- frontend/lib/api/axios.ts
+++ frontend/lib/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:5050";
 
 const getBrowserCookie = (name: string) => {
   if (typeof document === "undefined") {
